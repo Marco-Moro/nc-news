16aa0aa4d88ded9ed469b80e17473cd9840af1b2
Marco-moro <marcomoronese1991@gmail.com>
2025-06-11 16:39:17 +0100

    script conditional to fix husky

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 const endpoints = require("./endpoints.json");
 const db = require("./db/connection");
-
+const cors = require("cors");
 
 const { getApi } = require("./controllers/api.controller");
 const { getTopics } = require("./controllers/topics.controller");
@@ -14,7 +16,7 @@ const { getCommentsByArticleId } = require("./controllers/comments.controller");
 const { postCommentByArticleId } = require("./controllers/comments.controller");
 const { patchArticleById } = require("./controllers/articles.controller");
 const { deleteCommentById } = require("./controllers/comments.controller");
-
+const { handleCustomErrors, handlePSQLErrors, handle500 } = require("./errors");
 
 app.get("/api", getApi);
 app.get("/api/topics", getTopics);
@@ -25,24 +27,8 @@ app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 app.post("/api/articles/:article_id/comments", postCommentByArticleId);
 app.patch("/api/articles/:article_id", patchArticleById);
 app.delete("/api/comments/:comment_id", deleteCommentById);
+app.use(handleCustomErrors);
+app.use(handlePSQLErrors);
+app.use(handle500);
 
-
-app.use((err, request, response, next) => {
-  if (err.status) {
-    response.status(err.status).send({ msg: err.msg });
-  } else next(err);
-});
-
-app.use((err, request, response, next) => {
-  if (err.code === "22P02") {
-    response.status(400).send({ msg: "bad request" });
-  } else next(err);
-});
-
-app.use((err, request, response, next) => {
-  console.log("Unhandled error:", err);
-  response.status(500).send({ msg: "Internal Server Error" });
-});
-
-
-module.exports = app
+module.exports = app;
